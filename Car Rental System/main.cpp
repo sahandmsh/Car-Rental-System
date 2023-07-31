bc03b0c46758be5defeaf8b31bd700deb0317efa
Sahand Mosharafian <82970651+sahandmsh@users.noreply.github.com>
2023-07-31 12:34:24 -0700

    Initial Commit

---
+++ Car Rental System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Car Rental System
+//
+//  Created by Sahand Mosharafian on 7/31/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

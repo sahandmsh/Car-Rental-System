4a91b475cfd96655e77b2abfaf3a291c7270a774
Sahand Mosharafian <82970651+sahandmsh@users.noreply.github.com>
2023-08-02 16:13:39 -0700

    !

--- Car Rental System/main.cpp
+++ Car Rental System/main.cpp
@@ -3,12 +3,13 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <utility>
 #include "Car.hpp"
 using namespace std;
 
 class Car_Rental{
-    map <string , vector<car>> car_list;
-    vector <string> rented_cars_id;
+    map < string , vector<car> > car_list;
+    map < car , vector<pair<int,int>> > reserved_car;
 public:
     void add_car(string car_type, std::string make, std::string model, std::string color, int year){
         car_list[car_type].push_back(car(car_type+to_string(car_list[car_type].size()+1), make, model, color, year));
@@ -21,13 +22,29 @@ public:
             }
         }
     }
+    int convert_date (string s_date){
+        int s=0;
+        s = stoi(string (s_date.begin()+6,s_date.end()));
+        s = s*100 + stoi(string (s_date.begin(),s_date.begin()+2));
+        s = s*100 + stoi(string (s_date.begin()+3,s_date.begin()+5));
+        return s;
+    }
     void customer_request(){
-        show_car_list("NA");
-        int id;
-        cout<< "**Please enter the vehicle number from the list**";
-        cin>> id;
-//        if (find(rented_cars_id.begin(),rented_cars_id.end(),id)==rented_cars_id.end()){}
-        
+        pair <int , int> d;
+        d = get_customer_dates();
+        while (d.first>=d.second){
+            cout<< "Invalid entries! Please enter valid dates!\n";
+            d = get_customer_dates();
+        }
+        cout<<"Available cars for the selected time slot:\n";
+    }
+    pair <int, int> get_customer_dates(){
+        string s,e;
+        cout<<"Please enter your desired start date in the format mm/dd/yyyy (e.g., 01/12/2023)   ";
+        cin>>s;
+        cout<<"Please enter your desired end date in the format mm/dd/yyyy   (e.g., 01/15/2023)   ";
+        cin>>e;
+        return make_pair (convert_date(s) , convert_date(e));
     }
 };
 
@@ -38,6 +55,5 @@ int main(){
     ABC.add_car ("C", "Kia", "Forte", "Grey", 2022);
     ABC.add_car ("C", "Toyota", "Corolla", "Blue", 2022);
     ABC.show_car_list("NA");
-    
-    string a,b;
+//    ABC.customer_request();
 }
